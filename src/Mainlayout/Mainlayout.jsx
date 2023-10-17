e9388a7ea94fff2ce5836611592646a326d93c40
Saifulkhandaker <khsaiful67@gmail.com>
2023-10-17 22:00:17 +0600

    footer added

--- src/Mainlayout/Mainlayout.jsx
+++ src/Mainlayout/Mainlayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navber from '../components/Navber';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const Mainlayout = () => {
     return (
         <div>
             <Navber></Navber>
             <Outlet></Outlet>
+            <Footer></Footer>
             
         </div>
     );
